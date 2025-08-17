5060ea7744ae738fe934c5240aab11c85324cd81
Luna Aphelion <luna-aphelion@proton.me>
2025-08-17 19:32:10 +0100

    it works now

---
+++ main.py
@@ -0,0 +1,80 @@
+#TODO: write a main function that determines what function to call to get the required atributes
+#TODO: write a function that adds the syncronised lyrics to the specified file if possible, failback to just providing a seperate lyrics file
+#TODO: make script exit on api call failure, current behaviour will try to set the lyrics to "1"
+import requests
+import mutagen
+import sys
+import json
+
+from mutagen.id3 import ID3, SYLT, Encoding, USLT
+from mutagen.mp3 import MP3
+
+fileName = sys.argv[1]
+
+def api_call(songTitle, songArtist, songAlbum, songDuration):
+    songTitle = songTitle.replace(" ", "-")
+    songArtist = songArtist.replace(" ", "-")
+    songAlbum = songAlbum.replace(" ", "-")
+
+    url = f"https://lrclib.net/api/get?artist_name={songArtist}&track_name={songTitle}&album_name={songAlbum}&duration={songDuration}"
+
+    print(url)
+    
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            lyrics = list()
+            RawSyncedLyrics = ""
+            for line in response.json()["syncedLyrics"].splitlines():
+                RawSyncedLyrics = RawSyncedLyrics + line.replace(" ", "", 1) + "\n"
+            for line in response.json()["syncedLyrics"].splitlines():
+                x = line.split()[0]
+                x = x.replace('[', '')
+                x = x.replace(']', '')
+                seconds = round(float(x.split(':')[1])) + (int(x.split(':')[0]) * 60)
+                miliseconds = seconds * 1000
+
+                lyrics.append((line.split(' ', 1)[1], miliseconds))
+            return (lyrics, RawSyncedLyrics)
+        else:
+            print('error ', response.status_code)
+            return 1
+    except requests.exceptions.RequestException as e:
+        print('error ', e)
+        return 1
+    
+def Handle_MP3():
+    print("dingus")
+    audioTags = ID3(fileName)
+
+    songTitle = str(audioTags.get('TIT2'))
+    songArtist = str(audioTags.get('TPE1'))
+    songAlbum = str(audioTags.get('TALB'))
+
+    audio = MP3(fileName) # ID3 is only the tags and duration isn't stored in ID3 tags, we have to use streaminfo
+    songDuration = round(float(audio.info.pprint().split(", ")[4].split()[0])) # i get the distinct impression this is a fucking terrible way to do this, if anyone has a better idea please do let me know
+    
+    lyrics = api_call(songTitle, songArtist, songAlbum, songDuration)
+    RawSyncedLyrics = lyrics[1]
+
+    audioTags.setall('SYLT', [SYLT(
+        encoding=Encoding.UTF8,
+        lang="eng",
+        format=2,
+        type=1,
+        text=lyrics[0]
+    )])
+    # fuck musicolet, musicolet is my fucking opp
+    print(RawSyncedLyrics)
+    audioTags.setall('USLT', [USLT(
+        encoding=Encoding.UTF8,
+        lang="   ",
+        format=2,
+        type=1,
+        text=RawSyncedLyrics
+    )])
+    audioTags.save(v2_version=3)
+    return 0
+
+Handle_MP3()
